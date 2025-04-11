4860339493cdf2b520e27e357a3661eb867cec0e
abarolli <obarolli@gmail.com>
2025-04-11 15:03:08 -0700

    Refactor last_message function to simplify handling of chat_state and ensure consistent return type

--- src/local_general_chat_researcher/utils.py
+++ src/local_general_chat_researcher/utils.py
@@ -1,15 +1,23 @@
 from datetime import datetime
 from langchain_core.messages import BaseMessage
+from tavily import TavilyClient
 
 from local_general_chat_researcher.state import ChatState
 
 
-def last_message(chat_state: ChatState | dict) -> BaseMessage:
-    if isinstance(chat_state, dict):
-        return chat_state.get("messages")[-1]
+def last_message(chat_state: ChatState) -> BaseMessage:
+    return chat_state["messages"][-1]
     
-    return chat_state.messages[-1]
-
 
 def current_date():
     return datetime.now().strftime("%Y-%m-%d")
+
+
+def web_search(query: str, topic: str):
+    client = TavilyClient()
+    return client.search(
+        query,
+        topic=topic,
+        max_results=2,
+        include_raw_content=True
+    )
