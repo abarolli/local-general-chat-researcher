00ff02091ab1732dfe7a3cd762172264b4efa48f
abarolli <obarolli@gmail.com>
2025-04-11 15:03:29 -0700

    Refactor ChatState class to use TypedDict for improved type safety and remove unnecessary attributes

--- src/local_general_chat_researcher/state.py
+++ src/local_general_chat_researcher/state.py
@@ -1,12 +1,8 @@
 
-from dataclasses import dataclass
-from typing import Annotated
+from typing import Annotated, TypedDict
 
 from langgraph.graph.message import add_messages
 
-@dataclass
-class ChatState:
+class ChatState(TypedDict):
     messages: Annotated[list, add_messages]
-    last_user_prompt: str = ""
-    query: str = ""
-    topic: str = ""
+    last_user_prompt: str
